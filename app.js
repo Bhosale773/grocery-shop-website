812cb4371a980ff0ebf3b604b514d2bd0234d1db
Bhosale773 <pratikbhosale773@gmail.com>
2019-07-30 18:13:36 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/",function(req, res){
+    res.render("landing");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+app.get("*",function(req, res){
+    res.send("Page not found");
+})
+
+app.listen(process.env.PORT || 1000, function(){
+    console.log("Server started");
+});
---
+++ public/scripts/script.js
@@ -0,0 +1,15 @@
+$('.carousel').carousel({
+    pause: "false"
+});
+
+$("#carouselExampleFade #content a, .btn").on("click", function(event){
+    if(this.hash !== ""){
+        event.preventDefault();
+        const hash = this.hash;
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 65
+            }, 800
+        );
+    }
+});
