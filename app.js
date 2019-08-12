2d54a553b465b700a609b8b45d00d29a0ee0df83
Bhosale773 <pratikbhosale773@gmail.com>
2019-08-12 23:54:07 +0530

    add authentication

--- app.js
+++ app.js
@@ -6,38 +6,103 @@ var jaggeryProducts = require("./productsData/jaggeryProducts");
 var pulsesProducts = require("./productsData/pulsesProducts");
 var channaProducts = require("./productsData/channaProducts");
 var newProducts = require("./productsData/newProducts");
-
+var mongoose              = require("mongoose");
+var bodyParser            = require("body-parser");
+var User                  = require("./models/user");
+var passport              = require("passport");
+var LocalStrategy         = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+    
+mongoose.connect("mongodb://localhost:27017/grocerry_shop_db",{useNewUrlParser:true});
 
 app.set("view engine", "ejs");
+
 app.use(express.static(__dirname + "/public"));
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(require("express-session")({
+    secret: "This is secret code",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/",function(req, res){
     res.render("landing");
 });
 
-app.get("/products/rice", function(req, res){
+app.get("/products/rice",isLoggedIn, function(req, res){
     res.render("rice",{riceProducts:riceProducts});
 });
 
-app.get("/products/sugar", function(req, res){
+app.get("/products/sugar",isLoggedIn, function(req, res){
     res.render("sugar",{sugarProducts:sugarProducts});
 });
 
-app.get("/products/jaggery", function(req, res){
+app.get("/products/jaggery",isLoggedIn, function(req, res){
     res.render("jaggery",{jaggeryProducts:jaggeryProducts});
 });
 
-app.get("/products/pulses", function(req, res){
+app.get("/products/pulses",isLoggedIn, function(req, res){
     res.render("pulses",{pulsesProducts:pulsesProducts});
 });
 
-app.get("/products/channa", function(req, res){
+app.get("/products/channa",isLoggedIn, function(req, res){
     res.render("channa",{channaProducts:channaProducts});
 });
 
-app.get("/products/new", function(req, res){
+app.get("/products/new",isLoggedIn, function(req, res){
     res.render("new",{newProducts:newProducts});
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+}) ,function(req, res){
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+app.get("/signup", function(req, res){
+    res.render("signup");
+});
+
+app.post("/signup", function(req, res){
+    User.register(new User({username: req.body.username, fname: req.body.fname, lname: req.body.lname, email: req.body.email, contact: req.body.contact}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('signup');
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/");
+        });
+    });
 });
 
 
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -36,13 +36,25 @@ function scrollToID(id, speed) {
     $('html,body').animate({ scrollTop: targetOffset }, speed);
 }
 
-$("nav .n-list .n-item a").on('click', function () {
-	$("nav .n-list").find('li.active').removeClass("active");
-	$(this).parent('li').addClass("active");
-});
-
-if(window.location.pathname !== "/"){
-    $("nav .n-list .home").removeClass("active");
+if(window.location.pathname == "/"){
+    $("nav .n-list").find('li.active').removeClass("active");
+    $("nav .n-list .home").addClass("active");
+}
+else if(window.location.pathname == "/login"){
+    $("nav .n-list").find('li.active').removeClass("active");
+    $("nav .n-list .login").addClass("active");
+}
+else if(window.location.pathname == "/signup"){
+    $("nav .n-list").find('li.active').removeClass("active");
+    $("nav .n-list .signup").addClass("active");
+}
+else if(window.location.pathname == "/aboutus"){
+    $("nav .n-list").find('li.active').removeClass("active");
+    $("nav .n-list .aboutus").addClass("active");
+}
+else if(window.location.pathname == "/contactus"){
+    $("nav .n-list").find('li.active').removeClass("active");
+    $("nav .n-list .contactus").addClass("active");
 }
 
 // Get the modal
